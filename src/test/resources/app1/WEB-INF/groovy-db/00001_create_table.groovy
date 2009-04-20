7c791f745ddc475fba03bbca8f2c42e364a0ed20
jcai <jcai@fepss.com>
2009-04-20 07:34:11 +0000

    纠正idea下的错误提示

--- src/test/resources/app1/WEB-INF/groovy-db/00001_create_table.groovy
+++ src/test/resources/app1/WEB-INF/groovy-db/00001_create_table.groovy
@@ -1,5 +1,5 @@
 
-class AddColumnUserTaskReminds extends corner.services.migration.BaseMigration {
+class SampleCreateTable extends corner.services.migration.BaseMigration {
 	def void self_up(){
 		create_table("table_a");
 	}
